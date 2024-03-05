bc45023f7327b5c10e7fe262a1d2d20147cd86fb
SyedAbrarTirmizi <sabrar500@gmail.com>
2024-03-05 13:27:43 -0600

    Exercises No = 1.1, 1.2, 1.3, 1.4

---
+++ app.js
@@ -0,0 +1,14 @@
+console.log("Practice exercise 1.1");
+
+let practice = "Syed Abrar";
+
+console.log(practice);
+
+let age = "25";
+
+console.log(age + 5)
+console.log(25 + 5)
+console.log("25" + 5)
+console.log("25 + 5")
+console.log("age + 5")
+console.log(age + "5")
